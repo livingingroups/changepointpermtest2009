e0d1e1508b9647b89a3dd0f925313338acfe77b4
Brock <kbrock@ab.mpg.de>
2025-09-02 15:45:53 +0200

    checkpoint

--- inst/tinytest/test_compare_to_pej.R
+++ inst/tinytest/test_compare_to_pej.R
@@ -10,58 +10,56 @@ compare_to_pej <- function(tf, alpha, q, N, tol) {
   bz2 <- pej$bz2
   
   
-  xyt <- xyt2 <- cbind("x" = easting(tf), "y" = northing(tf), t = 1:length(inp[[1]]))
-  b_xyt <- xyt[NROW(xyt):1,]
-  # calcuate diff of coordinates
-  b_xy_diff <- structure(apply(b_xyt[, 1:2], 2, FUN = diff), dimnames = list(NULL,c("x_diff", "y_diff")))
-  # remove points at which animal stays still
-  ind_new <- c(TRUE, sqrt(b_xy_diff[, "x_diff"]^2 + b_xy_diff[, "y_diff"]^2) > tol)
-  b_xyt2 <- b_xyt[ind_new,]
+  # tf <- cbind("x" = easting(tf), "y" = northing(tf), t = 1:length(inp[[1]]))
+  # tf[nrow(tf):1,] <- tf[NROW(tf):1,]
+  # # calcuate diff of coordinates
+  # b_xy_diff <- structure(apply(tf[nrow(tf):1,][, 1:2], 2, FUN = diff), dimnames = list(NULL,c("x_diff", "y_diff")))
+  # # remove points at which animal stays still
+  # ind_new <- c(TRUE, sqrt(b_xy_diff[, "x_diff"]^2 + b_xy_diff[, "y_diff"]^2) > tol)
+  # tf_move <- tf[nrow(tf):1,][ind_new,]
 
 
   tf_move <- tf[c(TRUE, sqrt(diff(easting(tf))^2 + diff(northing(tf))^2) > tol),]
   # reverse it
   tf_move <- tf_move[nrow(tf_move):1, ]
-  # TODO: document change time as chai
+  # TODO: document change time as changes outcome
+  old_time <- time(tf_move)
   tf_move[, attr(tf_move, "time")] <- seq_len(nrow(tf_move))
-  set.seed(2025)
-  sig_rcpp <- change_point_test(tf_move, q = q, N = N, alpha = alpha, seed = 2025 )$sig #FIXME bz1 and bz2
-  # 89785563 0.94423821 0.93400825 0.97844700 0.06036534 0.04507917 0.83709193 0.48538282 0.85336106 0.59090350 0.82804696 0.79522473 0.47586021
-  # all.equal(u, sig_rcpp) #last runif is equal
-  
+  sig_rcpp <- change_point_test(tf_move, q = q, N = N, alpha = alpha, seed = 2025 )$sig
+
 
 
   sig_rcpp[1] <- 0
-  b_xyt2 <- cbind(b_xyt2,
+  tf_move[, attr(tf_move, "time")] <- old_time
+  tf_move <- cbind(tf_move,
                   "sig" = sig_rcpp,
                   "cp_no" = ifelse(sig_rcpp == 0, 0, cumsum(sig_rcpp)))
 
   # re-index - merge with b_xyt
-  b_xyt <- merge(b_xyt, b_xyt2[, c("t", "sig")], by = "t", all.x = TRUE, sort = FALSE)
+  tf <- merge(tf, tf_move[, c("time", "sig")], by = "time", all.x = TRUE, sort = FALSE)
   # b_xyt <- b_xyt[rev(order(b_xyt[,"t"])), ]
-  xyt <- b_xyt[order(b_xyt[,"t"]), ]
-  xyt[, "cp_no"] <- ifelse(is.na(xyt[, "sig"]), NA,
-                             ifelse(xyt[, "sig"] == 0, 0, cumsum(na.fill(xyt[, "sig"], fill = 0))))
+  tf <- tf[order(tf[ ,"time"]), ]
+  tf[, "cp_no"] <- ifelse(is.na(tf[, "sig"]), NA,
+                             ifelse(tf[, "sig"] == 0, 0, cumsum(na.fill(tf[, "sig"], fill = 0))))
   #na.locf
-  xyt[, "sig"] <- na.locf(xyt[, "sig"], fromLast = TRUE, na.rm = FALSE)
-  xyt[, "cp_no"] <- na.locf(xyt[, "cp_no"], fromLast = TRUE, na.rm = FALSE)
-  rownames(xyt) <- NULL
+  tf[, "sig"] <- na.locf(tf[, "sig"], fromLast = TRUE, na.rm = FALSE)
+  tf[, "cp_no"] <- na.locf(tf[, "cp_no"], fromLast = TRUE, na.rm = FALSE)
+  rownames(tf) <- NULL
 
 
-  xyt
-  xyt[which(xyt$sig == 1),]
+  tf
+  tf[which(tf$sig == 1),]
 
-  xyt_cp <- xyt[xyt$sig != 0,]
+  xyt_cp <- tf[tf$sig != 0,]
   xyt_cp_split <- split(xyt_cp, f = xyt_cp$cp_no)
 
   cps_rcpp <- do.call("rbind", lapply(xyt_cp_split, function(x) {
-    setNames(c(min(x$t), max(x$t), x$x[1], x$y[1]), c("first", "last", "north", "east"))
+    setNames(c(min(x$time), max(x$time), x$x1[1], x$x2[1]), c("first", "last", "north", "east"))
   }))
 
   rownames(cps_rcpp) <- NULL
-  #browser()
-  expect_equal(pej$bz1, b_xyt2[, "x"])
-  expect_equal(pej$bz2, b_xyt2[, "y"])
+  expect_equal(pej$bz1, tf_move[, "x1"])
+  expect_equal(pej$bz2, tf_move[, "x2"])
   expect_equal(pej$sig, sig_rcpp)
   expect_equal(pej$cps, cps_rcpp)
 
