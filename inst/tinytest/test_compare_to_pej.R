2325673a99893c80ab3bd1c01ce71660305d91f3
Brock <kbrock@ab.mpg.de>
2025-09-03 16:41:49 +0200

    checkpoint still passing

--- inst/tinytest/test_compare_to_pej.R
+++ inst/tinytest/test_compare_to_pej.R
@@ -3,11 +3,15 @@ library(tinytest)
 library(trackframe)
 library(cpt)
 
-pej_tf_filter <- \(tf, tol) tf[c(
-  abs(diff(easting(tf))) > tol &
-    abs(diff(northing(tf))) > tol,
-  TRUE
-), ]
+pej_tf_filter <- \(tf, tol) {
+  tf <- tf[c(
+    abs(diff(easting(tf))) > tol &
+      abs(diff(northing(tf))) > tol,
+    TRUE
+  ), ]
+  tf <- tf[rev(seq_len(nrow(tf))),]
+  tf
+}
 
 
 distance_filter  <- \(tf, tol) tf[c(sqrt(diff(easting(tf))^2 + diff(northing(tf))^2, TRUE) > tol), ]
@@ -22,27 +26,14 @@ compare_to_pej <- function(tf, alpha, q, N, tol) {
   )
   pej <- pej_implementation(easting(tf), northing(tf), alpha, q, N, tol) #nolint
 
-  # filter only when there is movement
-  tf_move <- pej_tf_filter(tf, tol)
 
-  # For some reason, doesn't work with time in df
-  # TODO: MRE and document why this is
-
-  rcpp <- change_point_test(tf_move, q = q, N = N, alpha = alpha, seed = 2025)
-
-  # combine back with the non-moving data
-  tf <- merge(tf, rcpp[, c(tf_colnames$time_col, "sig", "cp_no")], by = tf_colnames$time_col, all.x = TRUE, sort = FALSE)
-  tf <- do.call(as.trackframe, c(list(tf), tf_colnames))
-
-  # Reverse the numbers and apply them to the rows that were within tolerance
-  tf[!is.na(tf$cp_no) & tf$cp_no > 0, "cp_no"] <- rev(tf[!is.na(tf$cp_no) & tf$cp_no > 0, "cp_no"])
-  tf[, "cp_no"] <- na.locf(tf[, "cp_no"], fromLast = TRUE, na.rm = FALSE)
+  tf <- change_point_test(tf, q = q, N = N, alpha = alpha, seed = 2025, legacy_movement_criteria = TRUE)
 
   # convert cps into format pej outputs
   rownames(tf) <- NULL
   xyt_cp <- tf[tf$cp_no != 0, ]
   xyt_cp_split <- split(xyt_cp, f = xyt_cp$cp_no)
-  cps_rcpp <- do.call("rbind", lapply(rev(xyt_cp_split), function(x) setNames(
+  cps_rcpp <- do.call("rbind", lapply(xyt_cp_split, function(x) setNames(
     # pej returns indices, not times.
     # because of tol and how it's used, there may be more than one easting, northing
     # pej takes the last one
@@ -55,11 +46,14 @@ compare_to_pej <- function(tf, alpha, q, N, tol) {
 
   rownames(cps_rcpp) <- NULL
 
+  # filter only when there is movement and reverse
+  bztf <- pej_tf_filter(tf, tol)
+
   if(
   sum(
-  expect_equal(pej$bz1, rev(easting(tf_move)), info = unique_ids(tf)),
-  expect_equal(pej$bz2, rev(northing(tf_move)), info = unique_ids(tf)),
-  expect_equal(pej$sig, rev(rcpp$sig), info = unique_ids(tf)),
+  expect_equal(pej$bz1, easting(bztf), info = unique_ids(tf)),
+  expect_equal(pej$bz2, northing(bztf), info = unique_ids(tf)),
+  expect_equal(pej$sig, bztf$sig, info = unique_ids(tf)),
   expect_equal(pej$cps, cps_rcpp, info = unique_ids(tf))
   )
   == 8) browser()
